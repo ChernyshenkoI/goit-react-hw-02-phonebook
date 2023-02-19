1f358ed166d678161f86332f24659ecb7866c802
Iryna C <106169218+ChernyshenkoI@users.noreply.github.com>
2023-02-19 13:29:48 +0200

    finale version

---
+++ src/components/Contacts/Contacts.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { StyledBtn } from 'components/Button/Button';
+
+class Contacts extends Component {
+  render() {
+    const { nameList, onClick } = this.props;
+    return (
+      <ul>
+        {nameList.map(({ name, id, number }) => (
+          <li key={id}>
+            <p>
+              {name}: {number}{' '}
+              <StyledBtn onClick={() => onClick(id)} type="button">
+                Delete
+              </StyledBtn>
+            </p>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default Contacts;
+
+Contacts.propTypes = {
+  nameList: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      number: PropTypes.string,
+    })
+  ),
+  onClick: PropTypes.func,
+};
